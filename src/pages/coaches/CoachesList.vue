d69ab205f6d81f5d307062422fd25c6027b73ee6
nzkks <kkshanthosh@gmail.com>
2025-02-05 15:41:10 +1300

    CoachesList: Add a link to Register page

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,4 +1,25 @@
 <template>
   <section>Filter Coaches</section>
-  <section>List of coaches</section>
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register as Coach</router-link>
+    </div>
+    <ul>
+      List of coaches
+    </ul>
+  </section>
 </template>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
