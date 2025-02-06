9ebaa610a9b41d97c86444c2b7389874e792ff8a
nzkks <kkshanthosh@gmail.com>
2025-02-06 13:08:29 +1300

    In coaches namespaced store, add static coaches data

--- src/store/modules/coaches/index.js
+++ src/store/modules/coaches/index.js
@@ -4,7 +4,30 @@ import getters from './getters';
 
 export default {
   namespaced: true,
-  state() {},
+  state() {
+    return {
+      coaches: [
+        {
+          id: 'c1',
+          firstName: 'John',
+          lastName: 'Doe',
+          areas: ['frontend', 'backend', 'career'],
+          description:
+            "I'm John and I've worked as a freelance web developer for years. Let me help you become a developer as well!",
+          hourlyRate: 30,
+        },
+        {
+          id: 'c2',
+          firstName: 'Jane',
+          lastName: 'Williams',
+          areas: ['frontend', 'career'],
+          description:
+            'I am Jane and as a senior developer in a big tech company, I can help you get your first job or progress in your current role.',
+          hourlyRate: 30,
+        },
+      ],
+    };
+  },
   mutations,
   actions,
   getters,
