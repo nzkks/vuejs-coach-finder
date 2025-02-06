c465166ca1482392a403d02baf1950e755e25ff0
nzkks <kkshanthosh@gmail.com>
2025-02-06 19:00:23 +1300

    create base components and register them globally

---
+++ src/components/ui/BaseButton.vue
@@ -0,0 +1,78 @@
+<template>
+  <button v-if="!link" :class="mode">
+    <slot></slot>
+  </button>
+  <RouterLink v-else :to="to" :class="mode">
+    <slot></slot>
+  </RouterLink>
+</template>
+
+<script>
+import { RouterLink } from 'vue-router';
+
+export default {
+  props: {
+    mode: {
+      type: String,
+      required: false,
+      default: null,
+    },
+    link: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    to: {
+      type: String,
+      required: false,
+      default: '/',
+    },
+  },
+  components: {
+    RouterLink,
+  },
+};
+</script>
+
+<style scoped>
+button,
+a {
+  text-decoration: none;
+  padding: 0.75rem 1.5rem;
+  font: inherit;
+  background-color: #044895;
+  border: 1px solid #00259e;
+  color: white;
+  cursor: pointer;
+  border-radius: 30px;
+  margin-right: 0.5rem;
+  display: inline-block;
+}
+
+a:hover,
+a:active,
+button:hover,
+button:active {
+  background-color: #0552ab;
+  border-color: #00259e;
+}
+
+.flat {
+  background-color: transparent;
+  color: #044895;
+  border: none;
+}
+
+.outline {
+  background-color: transparent;
+  border-color: #044895;
+  color: #044895;
+}
+
+.flat:hover,
+.flat:active,
+.outline:hover,
+.outline:active {
+  background-color: #d2dcff;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -4,9 +4,17 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import BaseBadge from './components/ui/BaseBadge.vue';
+import BaseButton from './components/ui/BaseButton.vue';
+import BaseCard from './components/ui/BaseCard.vue';
+
 const app = createApp(App);
 
 app.use(store);
 app.use(router);
+
+app.component('base-badge', BaseBadge);
+app.component('base-button', BaseButton);
+app.component('base-card', BaseCard);
 
 app.mount('#app');
