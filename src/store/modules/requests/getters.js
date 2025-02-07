d3ff59da5d58c194f696ef1c6914d6f856004a3a
nzkks <kkshanthosh@gmail.com>
2025-02-08 10:36:44 +1300

    ReceivedRequests page: Show the requests

--- src/store/modules/requests/getters.js
+++ src/store/modules/requests/getters.js
@@ -1,1 +1,11 @@
-export default {};
+export default {
+  requests(state) {
+    return state.requests;
+  },
+  // hasRequests(_, getters) {
+  //   return getters.requests && getters.requests.length > 0;
+  // },
+  hasRequests(state) {
+    return state.requests && state.requests.length > 0;
+  },
+};
