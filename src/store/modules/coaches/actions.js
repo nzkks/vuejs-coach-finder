39d15c212826708ff597f28e31e6962543cb057e
nzkks <kkshanthosh@gmail.com>
2025-02-07 16:00:40 +1300

    Register coach: connect the form submission to the store

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,1 +1,14 @@
-export default {};
+export default {
+  registerCoach(context, payload) {
+    const newCoach = {
+      id: 'c3',
+      firstName: payload.firstName,
+      lastName: payload.lastName,
+      areas: payload.areas,
+      description: payload.description,
+      hourlyRate: payload.rate,
+    };
+
+    context.commit('registerCoach', newCoach);
+  },
+};
