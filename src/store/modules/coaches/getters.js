9eae7148c19848edcfda13a71fd135741e5f7409
nzkks <kkshanthosh@gmail.com>
2025-02-07 16:21:06 +1300

    Show the Register as Coach button o condition

--- src/store/modules/coaches/getters.js
+++ src/store/modules/coaches/getters.js
@@ -5,4 +5,10 @@ export default {
   hasCoaches(state) {
     return state.coaches && state.coaches.length > 0;
   },
+  isCoach(state, getters, rootState, rootGetters) {
+    // isCoach(_, getters, _2, rootGetters) { // if linting doesn't allow unused params in above line
+    const coaches = getters.coaches;
+    const userId = rootGetters.userId;
+    return coaches.some((coach) => coach.id === userId);
+  },
 };
