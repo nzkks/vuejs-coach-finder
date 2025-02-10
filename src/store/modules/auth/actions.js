eb27e0ff53a4e7ffc0af0ae2616c949ffea5528d
nzkks <kkshanthosh@gmail.com>
2025-02-10 22:10:12 +1300

    auth actions: refactor

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,56 +1,39 @@
 export default {
   async login(context, payload) {
-    const response = await fetch(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.VUE_APP_FIREBASE_WEBAPI_KEY}`,
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    );
-
-    const responseData = await response.json();
-
-    if (!response.ok) {
-      console.log(responseData);
-      const error = new Error(
-        responseData.error.message || 'Failed to authenticate!'
-      );
-      throw error;
-    }
-
-    context.commit('setUser', {
-      token: responseData.idToken,
-      userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn,
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'login',
     });
   },
   async signup(context, payload) {
-    const response = await fetch(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.VUE_APP_FIREBASE_WEBAPI_KEY}`,
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    );
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'signup',
+    });
+  },
+  async auth(context, payload) {
+    const mode = payload.mode;
+    let url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.VUE_APP_FIREBASE_WEBAPI_KEY}`;
+
+    if (mode === 'signup') {
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.VUE_APP_FIREBASE_WEBAPI_KEY}`;
+    }
+
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
 
     const responseData = await response.json();
 
     if (!response.ok) {
-      console.log(responseData);
       const error = new Error(
         responseData.error.message || 'Failed to authenticate!'
       );
